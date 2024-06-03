5580d72abe28a5411716733a8bb743baaf39474e
Hansel <hansel.hir@gmail.com>
2024-06-03 16:37:48 -0500

    add name cpacity provider

--- main.tf
+++ main.tf
@@ -255,7 +255,7 @@ resource "aws_autoscaling_group" "autoscaling_group" {
 // provider
 
 resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-#  name = "cp-ecs-1"
+  name = "provider-ecs"
   auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.autoscaling_group.arn
 
