8564dd72c788056d82c94fc53cee9448f94d7e3f
Hansel <hansel.hir@gmail.com>
2024-06-03 19:28:28 -0500

    change name task and provider

--- main.tf
+++ main.tf
@@ -255,7 +255,7 @@ resource "aws_autoscaling_group" "autoscaling_group" {
 // provider
 
 resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-  name = "provider-ecs-32"
+  name = "provider-ecs-33"
   auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.autoscaling_group.arn
 #    managed_termination_protection = "DISABLED"
@@ -283,7 +283,7 @@ resource "aws_ecs_cluster_capacity_providers" "this" {
 }
 
 resource "aws_ecs_task_definition" "ecs_task_definition" {
-  family             = "task-01-2"
+  family             = "task-01-3"
   network_mode       = "awsvpc"
   cpu                = "1 vCPU"
   memory             = "3 GB"
